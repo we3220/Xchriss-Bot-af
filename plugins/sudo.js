9d81dd4ae2ff20cc539c8fd33efcd9b43c2da81f
root <root@localhost.localdomain>
2025-04-05 21:55:52 +0100

    .

--- plugins/sudo.js
+++ plugins/sudo.js
@@ -57,7 +57,7 @@ Index({
 try {
    if (!message.isCreator) return message.reply("_Command is for bot owner only")
    let sudos = await getSudo()
-   console.log(Object.keys(sudos))
+   console.log(sudos)
    await message.reply(`Sudo numbers are \n${sudos}`)
 } catch (err) {                                           
 	message.reply(err.toString())
