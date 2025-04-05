ff2b174c272426cc7d1bb6b7700f64ab72233739
root <root@localhost.localdomain>
2025-04-05 11:30:01 +0100

    .

---
+++ plugins/nsfw.js
@@ -0,0 +1,313 @@
+const { cmd, mode } = require("../lib")
+const { NSFW } = require("nsfwhub")
+const nsfw = new NSFW()
+const Index = cmd
+Index({
+	pattern: 'ass',
+	react: "🥵",
+    desc: 'nsfw',
+	category: 'nsfw'
+}, async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("ass")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'sixnine',
+	react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("sixnine")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'pussy',
+	react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("pussy")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'dick',
+		react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("dick")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'anal',
+	react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("anal")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'boobs',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("boobs")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+
+} catch (error) {
+    message.reply(error.toString())
+}
+})
+
+Index({
+	pattern: 'bdsm',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("bdsm")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'black',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("black")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'easter',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("easter")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'bottomless',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("bottomless")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'blowjob',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("blowjob")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'cum',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("cum")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'cumsluts',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("cumsluts")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'sixnine',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("sixnine")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'dp',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("dp")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'dom',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("dom")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'extreme',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("extreme")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'feet',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("feet")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'finger',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("finger")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'fuck',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("fuck")
+        conn.sendMessage.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'kiss',
+	react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+try {
+const data = await nsfw.fetch("kiss")
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+} catch (error) {
+    message.reply(error.toString())
+}
+});
+
+Index({
+	pattern: 'lesbian',
+    react: "🥵",
+	desc: 'nsfw',
+	category: 'nsfw'
+},async (conn, message, args) => {
+//try {
+const data = await nsfw.fetch("lesbian")
+console.log(data)
+        conn.sendMessage(message.jid, { image: { url: data.image.url }, caption: "🥹🥵",  }, { quoted: message.card })
+/*} catch (error) {
+    message.reply(error.toString())
+}*/
+}); 
