6cbb95f05cf47817bc52ab694bbab32327f37185
root <root@localhost.localdomain>
2025-04-07 08:47:07 +0100

    .

--- plugins/group.js
+++ plugins/group.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 const path = require("path")
 const rept = JSON.parse(fs.readFileSync(path.resolve(__dirname,'./media/ban.json')))
 const Index = cmd
-
+const moment = require("moment")
 
 Index({
     pattern: 'getppgc',
